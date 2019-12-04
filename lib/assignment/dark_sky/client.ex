98d1c5c55486bcb9bde33694f928f69cb7760e12
Bohdan Nikolaienko <bohdan.nikolaienko@ext.airbnb.com>
2019-12-04 14:31:13 -0800

    Add typespec

--- lib/assignment/dark_sky/client.ex
+++ lib/assignment/dark_sky/client.ex
@@ -1,11 +1,16 @@
 defmodule Assignment.DarkSky.Client do
   use Tesla
 
+  @type coordinate :: String.t()
+  @type query_params :: [exclude: String.t, extend: String.t, lang: String.t, units: String.t]
+  @type result :: Tesla.Env.result()
+
   @key Application.fetch_env!(:assignment, :dark_sky_key)
 
   plug Tesla.Middleware.BaseUrl, "https://api.darksky.net/forecast/#{@key}/"
 
-  def forecast(latitude, longitude) do
-    get(latitude <> "," <> longitude)
+  @spec forecast(coordinate, coordinate, query_params) :: result
+  def forecast(latitude, longitude, query_params \\ []) do
+    get(latitude <> "," <> longitude, query: query_params)
   end
 end
