93867d667d5cb6ad2a91769143dfc576b283d33e
Bohdan Nikolaienko <23bohdan@gmail.com>
2019-12-05 00:42:45 -0800

    Implement a view to render forecast in needed format

---
+++ lib/assignment_web/views/forecast_view.ex
@@ -0,0 +1,31 @@
+defmodule AssignmentWeb.ForecastView do
+
+  def render("forecast", %{forecast: forecast}) do
+    %{ date: render_date(forecast["currently"]["time"]),
+       type: forecast["currently"]["precipType"],
+       description: forecast["currently"]["summary"],
+       temperature: forecast["currently"]["temperature"],
+       wind: %{
+         speed: forecast["currently"]["windSpeed"],
+         bearing: forecast["currently"]["windBearing"]
+       },
+       precipitation_probability: forecast["currently"]["precipProbability"],
+       daily: forecast["daily"]["data"] |> Enum.map(&render_daily/1)
+     }
+  end
+
+  defp render_daily(daily) do
+    %{ date: render_date(daily["time"]),
+       type: daily["precipType"],
+       description: daily["summary"],
+       temperature: %{
+         low: daily["temperatureMin"],
+         high: daily["temperatureMax"]
+       }
+     }
+  end
+
+  defp render_date(unix) do
+    DateTime.from_unix!(unix)
+  end
+end
