9c467e5cdf1a4393d4b2605594cd467b7c780209
Bohdan Nikolaienko <23bohdan@gmail.com>
2019-12-04 19:26:07 -0800

    Add base implementation of GraphQL API (#2)
    * Add base implementation of GraphQL API

---
+++ lib/assignment_web/schema.ex
@@ -0,0 +1,15 @@
+defmodule AssignmentWeb.Schema do
+  use Absinthe.Schema
+
+  import_types Absinthe.Type.Custom
+  import_types AssignmentWeb.Schema.WeatherTypes
+
+  alias AssignmentWeb.Resolvers
+
+  query do
+    field :weather_forecast, :forecast do
+      arg :input, non_null(:coordinate_input)
+      resolve &Resolvers.Weather.forecast/3
+    end
+  end
+end
