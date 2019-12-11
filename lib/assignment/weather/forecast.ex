65b5879cfc8610127a344ade0f0baa92f7c121bc
Bohdan Nikolaienko <bohdan.nikolaienko@ext.airbnb.com>
2019-12-10 17:41:50 -0800

    Update typespec

--- lib/assignment/weather/forecast.ex
+++ lib/assignment/weather/forecast.ex
@@ -1,13 +1,10 @@
 defmodule Assignment.Weather.Forecast do
   alias Assignment.DarkSky
 
-  @type body :: %{required(String.t()) => any()}
-  @type result :: {:ok, body} | {:error, String.t()}
-
   @currently_and_daily_query [exclude: "[minutely,hourly,alerts,flags]"]
 
-  @spec currently_and_daily(%Decimal{}, %Decimal{}) :: result
-  def currently_and_daily(latitude, longitude) do
-    DarkSky.Client.forecast(latitude, longitude, @currently_and_daily_query)
+  @spec currently_and_daily(DarkSky.Client.coordinates()) :: DarkSky.Client.result()
+  def currently_and_daily(coordinates) do
+    DarkSky.Client.forecast(coordinates, @currently_and_daily_query)
   end
 end
--- lib/assignment_web/resolvers/weather.ex
+++ lib/assignment_web/resolvers/weather.ex
@@ -2,6 +2,6 @@ defmodule AssignmentWeb.Resolvers.Weather do
   alias Assignment.Weather.Forecast
 
   def forecast(%{input: %{latitude: latitude, longitude: longitude}}, _) do
-    Forecast.currently_and_daily(latitude, longitude)
+    Forecast.currently_and_daily({latitude, longitude})
   end
 end
